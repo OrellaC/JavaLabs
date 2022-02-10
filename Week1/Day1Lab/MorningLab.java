15e648d20c3256c06b3796d2201d5944c31f304e
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-09 19:28:51 -0500

    Answered second question

--- Week1/Day1Lab/MorningLab.java
+++ Week1/Day1Lab/MorningLab.java
@@ -12,3 +12,38 @@ public class MorningLab
         System.out.println (num2 + "is greater");
     }
 }
+
+//  Write an if/else statement for the following requirements:
+// If student gets 90 or higher: console log  A
+// If students get 80 or above: console log B
+// If students get 70 or above: console log C
+// If students get 55 or above: console log D
+// Any grade lower than 55 is F
+public class Grade{
+    public static void main(String[] args){
+        int score;
+        char grade;
+
+        if (score >= 90)
+        {
+           grade = 'A';
+        }
+        else if (score >= 80)
+        {
+           grade = 'B';
+        }
+        else if (score >= 70)
+        {
+           grade = 'C';
+        }
+        else if (score >= 50)
+        {
+           grade = 'D';
+        }
+        else
+        {
+           grade = 'F';
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}

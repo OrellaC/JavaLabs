4b71426cbc67457b7f6852c2c1e1dbc404b6a2cd
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-09 19:12:34 -0500

    Created file/folder structure

---
+++ Week1/Day1Lab/MorningLab.java
@@ -0,0 +1,14 @@
+// Write a Java program that display the larger of two integers
+public class MorningLab 
+{
+    public static void main(String[]args)
+    {
+        int num1 = 50, num2 = 20;
+        if (num1 == num2)
+        System.out.println ("both are equal");
+        else if (num1 > num2)
+        System.out.println (num1 + " is greater");
+        else 
+        System.out.println (num2 + "is greater");
+    }
+}
